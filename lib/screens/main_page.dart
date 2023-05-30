354682e32dc8d30039d2b01406256626db24b661
jacob-macleod <jacob.e.macleod@gmail.com>
2023-05-30 22:53:13 +0100

    Added an image and button to the page

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -4,8 +4,28 @@ import 'package:flutter_quiz/componments/styled_text.dart';
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  void buttonPressed() {
+    print("Button Pressed");
+  }
+
   @override
   Widget build(context) {
-    return StyledText(myText: "Learn Flutter the fun way!");
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 40),
+          child: Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 250,
+          ),
+        ),
+        const StyledText(myText: "Learn Flutter the fun way!"),
+        TextButton(
+          onPressed: buttonPressed,
+          child: const Text("Hello World"),
+        ),
+      ],
+    );
   }
 }
