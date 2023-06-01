87ae5daede620e79668b1efaa78a836bf5e6a423
jacob-macleod <jacob.e.macleod@gmail.com>
2023-06-01 19:09:46 +0100

    Added actual styling to the buttons on the quiz page

---
+++ lib/componments/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.text, this.function, {super.key});
+  final String text;
+  final void Function() function;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: function,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: const Color.fromRGBO(3, 46, 74, 1),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/componments/styled_text.dart';
+import 'package:flutter_quiz/componments/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,29 +12,21 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  void onButtonClick() {
+    print("button clicked");
+  }
+
   @override
   Widget build(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const Text("The Question"),
+        const StyledText(myText: "The Question"),
         const SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text("Answer 1"),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text("Answer 1"),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text("Answer 1"),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text("Answer 1"),
-        ),
+        AnswerButton("Answer 1", onButtonClick),
+        AnswerButton("Answer 2", onButtonClick),
+        AnswerButton("Answer 3", onButtonClick),
+        AnswerButton("Answer 4", onButtonClick),
       ],
     );
   }
