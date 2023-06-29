8c79c336bd4e09ce5a3b2fb7666c181558c1fcbf
jacob-macleod <jacob.e.macleod@gmail.com>
2023-06-29 09:49:38 +0100

    Added a get selected answers function

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen(this.selectedAnswers, this.switchScreen, {super.key});
   final List<String>? selectedAnswers;
   final Function(String screen, {List<String>? selectedAns}) switchScreen;
 
+  List<Map<String, Object>> getSelectedAnswers() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < selectedAnswers!.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': selectedAnswers![i],
+        },
+      );
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(context) {
-    print(selectedAnswers);
     if (selectedAnswers == null) {
       return const Text("Error - no answers passed to Results Screen");
     }
